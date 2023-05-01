2ff4b2d8fe3902ee78056c21c3f11df3669039be
vincentkengo-ha <98455946+vincentkengo-ha@users.noreply.github.com>
2023-05-01 20:19:17 +0300

    navigation to Overview

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,5 +1,20 @@
-import { Text } from "react-native";
+import { Button, View, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-export const Menu = () => {
-  return <Text>Ooga booga</Text>;
+export const Menu = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Button 
+        title="Members of parliament"
+        onPress={() => navigation.navigate("Overview")}
+      />
+      <StatusBar style="auto"/>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
