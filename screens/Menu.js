9174cb752eb06f5d9425a80638ef0cf44aff655c
vincentkengo-ha <98455946+vincentkengo-ha@users.noreply.github.com>
2023-05-01 21:28:15 +0300

    logo

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,10 +1,14 @@
-import { Button, View, StyleSheet } from "react-native";
+import { Button, View, StyleSheet, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 export const Menu = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Button 
+      <Image
+        styles={styles.logo}
+        source={require("../assets/logo.png")}
+      />
+      <Button
         title="Members of parliament"
         onPress={() => navigation.navigate("Overview")}
       />
@@ -15,6 +19,14 @@ export const Menu = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
+    
     flex: 1
-  }
+  },
+  logo: {
+    // not resizing the image
+    width: null,
+    height: null,
+    resizeMode: 'contain',
+    objectFit: 'scale-down'
+  },
 })
